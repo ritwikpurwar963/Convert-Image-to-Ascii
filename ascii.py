1618f94ec53ca2b9073a671a3497557c443af10b
ritwikpurwar963 <124861288+ritwikpurwar963@users.noreply.github.com>
2023-08-12 14:34:34 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,3 @@
+import pywhatkit as kit
+
+kit.image_to_ascii_art("C:/Users/user/Downloads/20230801100709_[fpdl.in]_apple-silhouette-delicious-fruit-with-leaf-juicy-fruit-from-apple-tree-sweet-pulp_654766-469_normal.jpg","ascii_new")
