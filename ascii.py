0062ae6312a03ead595c7396628f78da481562f4
ritwikpurwar963 <124861288+ritwikpurwar963@users.noreply.github.com>
2023-08-12 14:41:11 +0530

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,3 +1,4 @@
 import pywhatkit as kit
-
-kit.image_to_ascii_art("C:/Users/user/Downloads/20230801100709_[fpdl.in]_apple-silhouette-delicious-fruit-with-leaf-juicy-fruit-from-apple-tree-sweet-pulp_654766-469_normal.jpg","ascii_new")
+image_path="C:/Users/user/Downloads/20230801100709_[fpdl.in]_apple-silhouette-delicious-fruit-with-leaf-juicy-fruit-from-apple-tree-sweet-pulp_654766-469_normal.jpg"
+kit.image_to_ascii_art(image_path,"ascii_new")
+print("Image Converted to Ascii Code")
